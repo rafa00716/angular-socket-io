28281b50f0d57d0c9c902bb2ef764875a402d93a
Luis <lmendez@neox.cl>
2024-11-27 16:18:08 -0300

    First POC

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { io } from "socket.io-client";
+
 
 @Component({
   selector: 'app-root',
@@ -8,6 +10,43 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-socket-io';
+  hostSocket = "http://localhost:3099";
+  authToken = "AUTH_TOKEN"
+  ngOnInit(): void {
+
+    const socket = io(this.hostSocket, {
+      withCredentials: true,
+      extraHeaders: {
+        'Authorization':`Bearer ${this.authToken}`
+      }
+    });
+  
+    socket.on("connect", () => {
+      const connectData = {
+        dateConnected: new Date(),
+        socketId: socket.id
+      }
+      
+      console.log({connectData});
+    });
+    
+    socket.on("disconnect", () => {
+      const disconnectData = {
+        dateDisconnected: new Date(),
+        socketId: socket.id
+      }
+      
+      console.log({disconnectData});
+    });
+
+    socket.on("event_name_1", (payloadEvent1) => {
+      console.log({payloadEvent1});
+    });
+
+    socket.on("event_name_2", (payloadEvent2) => {
+      console.log({payloadEvent2});
+    });
+  }
 }
